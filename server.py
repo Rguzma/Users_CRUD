a3227b68ccc5a669e5672e12c377a7c653477861
Your Name <you@example.com>
2021-10-04 06:29:36 -0600

    rguzma

--- server.py
+++ server.py
@@ -1,28 +1,60 @@
-from flask import Flask, render_template, request, redirect      #FIRST STEP
-from users import User                                      #4TH STEP
-app = Flask(__name__)
-@app.route('/')                                             #FIRST STEP
-def index():                                                #FIRST STEP 
-    return redirect('/users') 
-    
+from flask import Flask, render_template, request, redirect
 
-@app.route('/users')                                        #4TH STEP
+from users import User
+
+app=Flask(__name__)
+
+@app.route('/')
+def index():
+    return redirect('/users')
+
+
+@app.route('/users')
 def users():
-    return render_template("users.html", users=User.get_all())
+    return render_template("users.html",users=User.get_all())
 
-# @app.route('/create_friend', methods=["POST"])
-# def create_friend():
-#     # First we make a data dictionary from our request.form coming from our template.
-#     # The keys in data need to line up exactly with the variables in our query string.
-#     data = {
-#         "fname": request.form["fname"],
-#         "lname" : request.form["lname"],
-#         "occ" : request.form["occ"]
-#     }
-#     # We pass the data dictionary into the save method from the Friend class.
-#     Friend.save(data)
-#     # Don't forget to redirect after saving to the database.
-#     return redirect('/')
 
-if __name__ == "__main__":                                     #FIRST STEP
-    app.run(debug=True)                                        #FIRST STEP
+@app.route('/user/new')
+def new():
+    return render_template("new_user.html")
+
+@app.route('/user/create',methods=['POST'])                    
+def create():
+    print(request.form)
+    User.save(request.form)
+    return redirect('/users')
+
+
+
+                                         #2 ETAPA
+@app.route('/user/edit/<int:id>')
+def edit(id):
+    data ={
+        "id":id
+    }
+    return render_template("edit_user.html",user=User.get_one(data))
+
+@app.route('/user/show/<int:id>')
+def show(id):
+    data ={ 
+        "id":id
+    }
+    return render_template("show_user.html",user=User.get_one(data))
+
+@app.route('/user/update',methods=['POST'])
+def update():
+    User.update(request.form)
+    return redirect('/users')
+
+@app.route('/user/destroy/<int:id>')
+def destroy(id):
+    data ={
+        'id': id
+    }
+    User.destroy(data)
+    return redirect('/users')
+
+
+if __name__=="__main__":
+    app.run(debug=True)
+
